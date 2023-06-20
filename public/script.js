e3bdba160b81a159e04f23572e4a2219e0377b11
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-06-20 10:19:42 +0900

    表示まで

--- public/script.js
+++ public/script.js
@@ -1,10 +1,27 @@
-const getAllThread = async() => {
-  try{
+const formDOM = document.querySelector(".thread-section");
+
+const getAllThread = async () => {
+  try {
     let allThreads = await axios.get("http://localhost:60190/api/v1/threads");
     console.log(allThreads);
-  }catch(error){
+    let { data } = allThreads;
+    console.log(data);
+    allThreads = data.map((thread) => {
+      const { title, content } = thread;
+      console.log(title, content);
+      return `
+      <div class="single-thread">
+        <h3>${title}</h3>
+        <p>${content}</p>
+      </div>
+      `;
+    });
+    formDOM.innerHTML = allThreads;
+  } catch (error) {
     console.log(error);
   }
 };
 
 getAllThread();
+
+//postメソッド
